bb1fe1b067c565eb19a76c36432ee0dfd77de078
William Vivas <williamvivasdev@gmail.com>
2019-08-09 18:31:01 -0400

    added phone/userinfo

--- server.js
+++ server.js
@@ -2,20 +2,30 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-const mongoose = require('mongoose');
+const config = require('config');
 const bodyparser = require('body-parser');
 const path = require('path');
-const db = require('./config/keys.js').mongoURI;
 
-mongoose.connect(db, { 
-    useNewUrlParser: true
-    
+const db = config.get('mongoURI');
+
+app.use(express.json());
+
+
+// Connect to Mongo
+mongoose
+  .connect(db, { 
+    useNewUrlParser: true,
+    useCreateIndex: true
   }) // Adding new mongo url parser
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-// console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+//routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 
 // create a GET route
 app.get('/express_backend', (req, res) => {

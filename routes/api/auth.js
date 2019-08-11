2e2d5632a05d2710af3aadfb54b9272b9ea0a668
William Vivas <williamvivasdev@gmail.com>
2019-08-11 14:58:07 -0400

    Finished validation

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,42 +8,94 @@ const auth = require('../../middleware/auth')
 
 
 router.post('/', (req, res) => {
-    const { email, password } = req.body;
-        
-    // Simple validation
-    if(!email || !password) {
-      return res.status(400).json({ msg: 'whats up' });
-    }
+  const {
+    email,
+    password,
+    userType
+  } = req.body;
+console.log(userType)
   
-    // Check for existing user
-    User.findOne({ email })
-      .then(user => {
-        if(!user) return res.status(400).json({ msg: 'User Does not exist' });
-       
-       bcrypt.compare(password, user.password)
-       .then(isMatch => {
-           if(!isMatch) return res.status(400).json({msg: "invalid Credentials"});
 
-           jwt.sign(
-            { id: user.id },
-            config.get('jwtSecret'),
-            { expiresIn: 3600 },
+
+
+// Simple validation
+  if (!email || !password) {
+    return res.status(400).json({
+      msg: 'whats up'
+    });
+  }
+  
+  // Check for existing user
+  User.findOne({
+      email
+    })
+    .then(user => {
+      if (!user) return res.status(400).json({
+        msg: 'User Does not exist'
+      });
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) return res.status(400).json({
+            msg: "invalid Credentials"
+          });
+
+          jwt.sign({
+              id: user.id
+            },
+            config.get('jwtSecret'), {
+              expiresIn: 3600
+            },
             (err, token) => {
-                if(err) throw err;
-                res.json({
-                    token,
-                   user:{
-                      id: user.id,
-                      name: user.name,
-                      email: user.email
-
-                    }
-                });
-              }
-            )
-          })
+              if (err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email,
+                  userType: user.userType
+                }
+               
+              
+              });
+            
+             
+            }
+          )
+       
+          
+        })
+    
+    
+    
+    
       })
-  });
+
+  
+      function pageRender(userType){
+        if(userType === "Barber"){
+          res.send("hello Barber")
+          console.log('hello')
+       
+       
+          if (userType === "client"){
+          res.send("hello client")
+        }
+          
+      
+          }
+      };
+  
+  
+  
+  
+      pageRender(userType); 
+  
+  
 
 
-            module.exports = router;
+});
+
+
+module.exports = router;
